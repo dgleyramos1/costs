091ef923fa2d80b21523aae8bb5a92f7ed139b8b
Dgley ramos <dgleysilva@gmail.com>
2022-05-05 01:21:05 -0300

    Estruturando página de criação de projetos

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Projectform from '../project/ProjectForm';
+import styles from './NewProject.module.css';
 
 const Newproject = () => {
     return (
-        <div>
-            <h1>NewProject</h1>
+        <div className={styles.newproject_container}>
+            <h1>Criar projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <Projectform/>
         </div>
     );
 }
