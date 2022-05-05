454b50df8e9a2fdb11ad2665c6249524e6effc45
Dgley ramos <dgleysilva@gmail.com>
2022-05-04 23:31:28 -0300

    Estruturando projeto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+import Container from './components/layout/Container';
+
 
 const App = () => {
-  return (
-    <div className="App">
-      <p>
-        costs
-      </p>
-    </div>
-  );
+    return (
+      <Router>
+        <div>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/contact">Contato</NavLink>
+          <NavLink to="/company">Company</NavLink>
+          <NavLink to="/newproject">New Project</NavLink>
+        </div>
+
+        <Routes>
+          <Container customClass="min-height">
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/company'>
+              <Company />
+            </Route>
+            <Route exactpath='/contact'>
+              <Contact />
+            </Route>
+            <Route exac={true} path='/newproject'>
+              <NewProject />
+            </Route>
+          </Container>
+        </Routes>
+        <p>Footer</p>
+      </Router>
+    )
 }
 
 export default App;
---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Newproject = () => {
+    return (
+        <div>
+            <h1>NewProject</h1>
+        </div>
+    );
+}
+
+export default Newproject;
