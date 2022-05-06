26889ddd2668a15df5fb6922a9190bf38f477f3b
Dgley ramos <dgleysilva@gmail.com>
2022-05-05 23:15:29 -0300

    Removendo serviços

--- src/components/service/ServiceCard.js
+++ src/components/service/ServiceCard.js
@@ -4,8 +4,9 @@ import { BsFillTrashFill } from 'react-icons/bs'
 
 const Servicecard = ({ id, name, cost, description, handleRemove }) => {
 
-    const remove = () => {
-
+    const remove = (e) => {
+        e.preventDefault()
+        handleRemove(id, cost)
     }
 
 
